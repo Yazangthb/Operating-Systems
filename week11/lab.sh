773441729b1134c79dc922f4cf323e2ddbec9b37
Yazangthb <118068414+Yazangthb@users.noreply.github.com>
2023-11-21 21:31:06 +0300

    Add files via upload

--- week11/lab.sh
+++ week11/lab.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
-fallocate -l 5000M lofs.img
-losetup -f lofs.img
-mkfs -t ext4 lofs.img
-sudo rm -r ./lofsdisk
-mkdir -p ./lofsdisk
-sudo mount -o loop lofs.img ./lofsdisk
+fallocate -l 50M lofs.img #Create a 50 MB image file named lofs.img using fallocate.
+losetup -f lofs.img #Associate the image file with a loop device using losetup.
+mkfs -t ext4 lofs.img #Create an ext4 file system on the image file using mkfs.
+sudo rm -r ./lofsdisk #Remove the existing lofsdisk directory if it exists
+mkdir -p ./lofsdisk #create a new directory
+sudo mount -o loop lofs.img ./lofsdisk #Mount the loop device to the lofsdisk directory.
+
+#Create two text files (file11 and file2) with specific content in the lofsdisk directory.
 echo "Yazan" > ./lofsdisk/file11
 echo "Alnakri" > ./lofsdisk/file2
 
-
+#ldd $1: This command prints the shared libraries required by the specified executable ($1). 
+#The awk command processes the output of ldd to extract the paths of the shared libraries
+#The sed command is used to replace the trailing comma (,) with a newline character (\n). The reason for this is to format the output
 get_libs() {
     ldd $1 | awk 'BEGIN{ORS=" "}$1~/^\//{print $1}$3~/^\//{print $3}' | sed 's/,$/\n/'
 }
+
 # Specify the loop file system directory
 lofsdisk="./lofsdisk"
 
@@ -26,7 +31,6 @@ mkdir -p ./lofsdisk/lib64
 # Loop through the commands and copy their shared libraries to the loop file system
 for cmd in "${commands[@]}"; do
     libs=$(get_libs "$(which $cmd)")
-    echo "hi:"
     echo "$(which $cmd)"
 	cp "$(which $cmd)" ./lofsdisk/bin
     # Copy shared libraries to the loop file system
@@ -40,13 +44,24 @@ for cmd in "${commands[@]}"; do
         fi
     done
 done
+
+#Copy an executable file named ex1 to the loop file system and perform additional file operations.
 cp ./ex1 ./lofsdisk/bin/ex1
 touch ./lofsdisk/bin/ex1.txt
 chmod +w ./lofsdisk/bin/ex1.txt
 
+#Use chroot to change the root directory to lofsdisk and execute a command (bin/ex1 > bin/ex1.txt) within that environment.
 sudo chroot ./lofsdisk /bin/bash -c "bin/ex1 > bin/ex1.txt"
 rm ex1.txt
 touch ex1.txt
+
+
+#Perform cleanup operations and copy the results to the host system.
 cp ./lofsdisk/bin/ex1.txt ./ex1.txt
+echo " ">>ex1.txt
+echo " ">>ex1.txt
+echo " ">>ex1.txt
+
+#Add the results of the ex1 when the root is not changed
 ./ex1 >> ex1.txt
 
